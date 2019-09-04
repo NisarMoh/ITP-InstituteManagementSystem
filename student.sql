6a368ee95a978c1db12c0090e7687ea514c2f978
Sarangan07 <53599378+Sarangan07@users.noreply.github.com>
2019-09-04 12:07:57 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,38 @@
+CREATE TABLE student(
+     
+   id VARCHAR(7) NOT NULL PRIMARY KEY DEFAULT '0',
+   username  varchar(50) NOT NULL,
+   password  varchar(100) NOT NULL,
+   firstname  varchar(100) NOT NULL,
+   lastname  varchar(100) NOT NULL,
+   gender varchar(50),
+   age int(100) NOT NULL,
+   contactno int(100) NOT NULL,
+   email varchar(100) NOT NULL,
+   address  varchar(250) NOT NULL,
+   
+  
+   
+ 
+);
+CREATE TABLE student1
+(
+  StudentID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+
+CREATE TABLE student
+(
+  id VARCHAR(7) NOT NULL PRIMARY KEY DEFAULT '0',
+);
+Now the trigger
+
+DELIMITER $$
+CREATE TRIGGER student1_student
+BEFORE INSERT ON student
+FOR EACH ROW
+BEGIN
+  INSERT INTO student1 VALUES (NULL);
+  SET NEW.id= CONCAT('STD', LPAD(LAST_INSERT_ID(), 4, '0'));
+END$$
+DELIMITER ;
