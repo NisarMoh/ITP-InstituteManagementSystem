5474ff7d0fdd742548c9d9f9728bf79cf5bbe1b4
HP <rajinthacorera@gmail.com>
2019-09-04 15:10:31 +0530

    Error 404 :(

--- 7. timetable_management [IT18508338]/timetable_admin_mainmenu_page.php
+++ 7. timetable_management [IT18508338]/timetable_admin_mainmenu_page.php
@@ -1,3 +1,15 @@
+<?php
+// Initialize the session
+
+session_start();
+include_once '../3. staff_management [IT18068610]/user.php';
+$user = new User();
+$Staff_ID = $_SESSION['Staff_ID'];
+if (!$user->get_session()) {
+    header("location:login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
